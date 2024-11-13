247b6fd4636534076dc05ed3fefaaa6607780fc6
johnsonl44 <johnsonl44@udayton.edu>
2024-11-13 16:37:42 -0500

    finished first set of slides

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,6 +4,16 @@ extends CharacterBody2D
 @export var run_speed: float = 150
 @export var jump_speed: float = -300
 
+@export var life: int =3:
+	set(value):
+		life = value
+		life_changed.emit(life)
+		if life<=0:
+			change_state(DEAD)
+
+signal life_changed(life: int)
+signal died
+
 enum {IDLE,RUN,JUMP,HURT,DEAD}
 var state:int = IDLE
 
@@ -28,12 +38,16 @@ func change_state(new_state: int) -> void:
 			$AnimationPlayer.play("run")
 		HURT:
 			$AnimationPlayer.play("hurt")
+			velocity.y = -200
+			velocity.x = -100 * sign(velocity.x)
 		JUMP:
 			$AnimationPlayer.play("jump_up")
 		DEAD:
 			hide()
 
 func get_input() -> void:
+	if state == HURT:
+		return
 	var right: bool = Input.is_action_pressed("right")
 	var left: bool = Input.is_action_pressed("left")
 	var jump: bool = Input.is_action_pressed("jump")
@@ -62,9 +76,13 @@ func get_input() -> void:
 	
 	if state == JUMP and velocity.y>0:
 		$AnimationPlayer.play("jump_down")
-	
+
+func hurt() -> void:
+	if state != HURT:
+		change_state(HURT)
+
 func reset(_position: Vector2) -> void:
 	position = _position
 	show()
 	change_state(IDLE)
-	
+	life = 0
