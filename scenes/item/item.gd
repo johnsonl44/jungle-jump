4677e8be272187da91d85abd4659ce9125f09dfe
johnsonl44 <johnsonl44@udayton.edu>
2024-11-18 09:28:59 -0500

    added item scene

---
+++ scenes/item/item.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+signal picked_up
+
+var textures: Dictionary ={
+	"cherry":
+		"res://assets/sprites/cherry.png",
+	"gem":
+		"res://assets/sprites/gem.png"
+}
+
+func init(item_type:String,_position:Vector2) ->void:
+	$Sprite2D.texture = load(textures[item_type]) as Texture
+	position = _position
+
+
+func _on_body_entered(body: Node2D) -> void:
+	picked_up.emit()
+	queue_free()
